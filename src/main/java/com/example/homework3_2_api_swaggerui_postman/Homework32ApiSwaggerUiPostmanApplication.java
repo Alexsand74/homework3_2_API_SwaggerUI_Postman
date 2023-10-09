bb9ad7b977183962e101bd7b52b23946421eadf1
Alexsand74 <1974alexsand@gmail.com>
2023-10-09 18:39:24 +0300

    Homework 3.2 will be here.

---
+++ src/main/java/com/example/homework3_2_api_swaggerui_postman/Homework32ApiSwaggerUiPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework3_2_api_swaggerui_postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework32ApiSwaggerUiPostmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework32ApiSwaggerUiPostmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework3_2_api_swaggerui_postman/Homework32ApiSwaggerUiPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework3_2_api_swaggerui_postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework32ApiSwaggerUiPostmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
